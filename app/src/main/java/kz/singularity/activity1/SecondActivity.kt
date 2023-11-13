d461bbc87380fe86203d8b1eeb678447f2a06f6a
Aslan Assylkhanov <AssylkhanovAslan@gmail.com>
2023-11-13 20:11:27 +0600

    Inital commit

---
+++ app/src/main/java/kz/singularity/activity1/SecondActivity.kt
@@ -0,0 +1,13 @@
+package kz.singularity.activity1
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
