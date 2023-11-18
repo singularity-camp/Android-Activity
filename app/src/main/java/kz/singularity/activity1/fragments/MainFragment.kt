0ce3e718af0a28a6f6010746f37c98c3d5683b34
Aslan Assylkhanov <AssylkhanovAslan@gmail.com>
2023-11-18 13:17:27 +0600

    Added lifecycle callbacks

--- app/src/main/java/kz/singularity/activity1/fragments/MainFragment.kt
+++ app/src/main/java/kz/singularity/activity1/fragments/MainFragment.kt
@@ -1,6 +1,37 @@
 package kz.singularity.activity1.fragments
 
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import kz.singularity.activity1.R
 
-class MainFragment : Fragment(R.layout.fragment_main)
+class MainFragment : Fragment() {
+
+    val TAG = "MainFragment"
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+
+    lateinit var tvName: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val contentView = inflater.inflate(R.layout.fragment_main, container, false)
+
+        return contentView
+    }
+}
