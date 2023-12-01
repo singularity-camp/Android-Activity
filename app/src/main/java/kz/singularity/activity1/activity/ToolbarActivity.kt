d5fd833f607fa27e5b5f678bdf594362d6e42d8d
Aslan Assylkhanov <AssylkhanovAslan@gmail.com>
2023-12-01 21:27:22 +0600

    Added optionsMenu

--- app/src/main/java/kz/singularity/activity1/activity/ToolbarActivity.kt
+++ app/src/main/java/kz/singularity/activity1/activity/ToolbarActivity.kt
@@ -2,10 +2,13 @@ package kz.singularity.activity1.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import kz.singularity.activity1.R
+import kz.singularity.activity1.showToast
 
 class ToolbarActivity : AppCompatActivity() {
 
@@ -23,6 +26,21 @@ class ToolbarActivity : AppCompatActivity() {
     private fun assignViews() {
         toolbar = findViewById(R.id.toolbar)
         btnOpen = findViewById(R.id.btn_open)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_android) {
+            showToast("Android clicked")
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     private fun setupClickListeners() {
