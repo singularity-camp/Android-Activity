1ab8215767e4d8e39f5df8f1f0a1ba3f21bd5f65
Aslan Assylkhanov <AssylkhanovAslan@gmail.com>
2023-12-02 11:21:09 +0600

    Added drawer activity from example

---
+++ app/src/main/java/kz/singularity/activity1/activity/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package kz.singularity.activity1.activity.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
