027a45d64514c4fe7f1a471ea1774d8aa491e24e
Aslan Assylkhanov <AssylkhanovAslan@gmail.com>
2023-12-06 20:03:07 +0600

    Completed viewpager 1

--- app/src/main/java/kz/singularity/activity1/viewpager/MyPagerAdapter.kt
+++ app/src/main/java/kz/singularity/activity1/viewpager/MyPagerAdapter.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.viewpager.widget.PagerAdapter
-import androidx.viewpager.widget.ViewPager
 import kz.singularity.activity1.R
 
 class MyPagerAdapter : PagerAdapter() {
--- app/src/main/java/kz/singularity/activity1/viewpager/ViewPagerOneActivity.kt
+++ app/src/main/java/kz/singularity/activity1/viewpager/ViewPagerOneActivity.kt
@@ -2,8 +2,11 @@ package kz.singularity.activity1.viewpager
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.OnClick
@@ -13,6 +16,15 @@ class ViewPagerOneActivity : AppCompatActivity() {
 
     @BindView(R.id.vp_main)
     lateinit var vpMain: ViewPager
+
+    @BindView(R.id.tv_indicator)
+    lateinit var tvIndicator: TextView
+
+    @BindView(R.id.btn_left)
+    lateinit var btnLeft: Button
+
+    @BindView(R.id.btn_right)
+    lateinit var btnRight: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +36,37 @@ class ViewPagerOneActivity : AppCompatActivity() {
 
     private fun setupViewPager() {
         vpMain.adapter = MyFragmentPagerAdapter(supportFragmentManager)
+        vpMain.addOnPageChangeListener(object : OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                setIndicatorText(position)
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+        })
+        setIndicatorText(vpMain.currentItem)
     }
+
+    private fun setIndicatorText(currentPosition: Int) {
+        tvIndicator.text = "${currentPosition + 1}/3"
+    }
+
+    @OnClick(R.id.btn_left)
+    fun onLeftClick() {
+        vpMain.setCurrentItem(vpMain.currentItem - 1)
+    }
+
+    @OnClick(R.id.btn_right)
+    fun onRightClick() {
+        vpMain.setCurrentItem(vpMain.currentItem + 1)
+    }
+
 }
