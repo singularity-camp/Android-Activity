05de19b200d34ccd3945f747453c1344a0bfc54c
Aslan Assylkhanov <AssylkhanovAslan@gmail.com>
2023-12-19 20:27:01 +0600

    Motion layout added

---
+++ app/src/main/java/kz/singularity/activity1/activity/MotionActivity.kt
@@ -0,0 +1,7 @@
+package kz.singularity.activity1.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import kz.singularity.activity1.R
+
+class MotionActivity : AppCompatActivity(R.layout.activity_motion) {
+}
